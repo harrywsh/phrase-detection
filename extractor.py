0b1176dab41923c32ab223e99b799996be2835f5
Shuhan Wang <shuhanw@wirelessprv-10-192-62-153.near.illinois.edu>
2019-09-27 20:30:53 -0500

    extract from whole document

--- extractor.py
+++ extractor.py
@@ -4,28 +4,33 @@ from spacy.matcher import PhraseMatcher
 
 nlp = spacy.load('en_core_web_sm')
 
-def extractor(text, pattern):
+def extractor(file, *pattern):
     '''
-    @param str text: text source for tuple extraction
+    @param str file: text file for tuple extraction
     @param list pattern: extraction pattern
     A phrase extractor based on spacy
     '''
+    phrases = []
     matcher = Matcher(nlp.vocab)
-    matcher.add("test", None, pattern)
-    doc = nlp(text)
-    matches = matcher(doc)
-    for match_id, start, end in matches:
-        string_id = nlp.vocab.strings[match_id]  # Get string representation
-        span = doc[start:end]  # The matched span
-        print(match_id, string_id, start, end, span.text)
+    matcher.add("pattern extraction", None, *pattern)
+    with open(file, "r") as f:
+        text = f.read().lower()
+        doc = nlp(text)
+        matches = matcher(doc)
+        for match_id, start, end in matches:
+            string_id = nlp.vocab.strings[match_id]  # Get string representation
+            span = doc[start:end]  # The matched span
+            print(match_id, string_id, start, end, span.text)
+            phrases.append(span.text)
+        return phrases
 
 def main():
-    pattern = [{"POS": "NOUN"}, {"POS": "NOUN"}]
-    with open("test.txt", "r") as f:
-        lines = f.readlines()
-        for line in lines:
-            print(line)
-            extractor(line, pattern)
+    # pattern1 = [{"POS": "NOUN"}, {"POS": "NOUN"}]
+    pattern2 = [{"POS": "ADJ"}, {"POS": "NOUN"}, {"POS": "NOUN"}]
+    # pattern = [{"POS": "NOUN"}, {"LOWER": "learning"}]
+    # pattern = [{"POS": "ADJ"}, {"LOWER": "learning"}]
+    p = extractor("test.txt", pattern2)
+    print(p)
 
 if __name__ == '__main__':
     main()
