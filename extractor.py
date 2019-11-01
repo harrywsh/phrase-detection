fcaf514805b8dba2982540ddb74df64fa9443bff
Shuhan Wang <shuhanw2@illinois.edu>
2019-11-01 06:00:41 -0500

    construct context graph

--- extractor.py
+++ extractor.py
@@ -13,6 +13,11 @@ from whoosh.query import Every
 # library for list manipulation
 import itertools
 
+# library for context graph
+import networkx as nx
+import matplotlib.pyplot as plt
+import numpy as np
+
 nlp = spacy.load('en_core_web_sm')
 
 def extractor(document, *pattern):
@@ -43,6 +48,22 @@ def partition(file, size = 1000000):
         yield data
 
 #%%
+def getPhrases(file, context_pattern):
+    new_phrases = set()
+    with open(file, 'r') as f:
+        t = f.read().lower()
+        matcher = Matcher(nlp.vocab)
+        doc = nlp(t)
+        for cp in context_pattern:
+            matcher.add("extraction", None, cp)
+            matches = matcher(doc)
+            for match_id, start, end in matches:
+                span = doc[start+2:end].text
+                if span not in new_phrases:
+                    new_phrases.add(span)
+            matcher.remove("extraction")
+    return new_phrases
+
 def patternSearch(T_0, file):
     phrase_patterns = set()
     seed_pattern = [nlp(x) for x in T_0]
@@ -57,8 +78,7 @@ def patternSearch(T_0, file):
             if p not in phrase_patterns:
                 phrase_patterns.add(p)
     # find patterns around seed phrases
-    content_pattern = []
-    context_pattern = []
+    unranked_patterns = []
     with open(file, "r") as f:
         text = nlp(f.read().lower())
         for phrase_pattern in phrase_patterns:
@@ -66,50 +86,66 @@ def patternSearch(T_0, file):
             end = phrase_pattern[1]
             if (text[start - 1].text == '\n'):
                 continue
-            # # add content pattern 
+            # add context pattern 
             tmp = []
             for i in range(2, 0, -1):
                 tmp.append({"TEXT": text[start - i].text})
-            # tmp.append({"TEXT": {"REGEX": "[a-zA-Z0-9_]"}, "OP":"+"})
+            # add content pattern 
             span = text[start:end]
             for token in span:
                 tmp.append({"POS": token.pos_})
-            # for i in range(3):
-            #     tmp.append({"TEXT": text[end + i].text})
-            #     if text[end + i].text == '\n':
-            #         break
-            if tmp not in context_pattern:
-                context_pattern.append(tmp)
+            if tmp not in unranked_patterns:
+                unranked_patterns.append(tmp)
                 print(tmp)
-
-    # get new phrases
-    return context_pattern
-
-def phraseExtraction(file, context_pattern):
-    new_phrases = set()
-    with open('test.txt', 'r') as f:
+    unranked_phrases = list(getPhrases(file, unranked_patterns))
+    # build context graph
+    context_graph = nx.Graph()
+    # add tuples and patterns into graph
+    for i in range(len(unranked_phrases)):
+        node = 't' + str(i)
+        context_graph.add_node(node, pos=(0, i))
+    for i in range(len(unranked_patterns)):
+        node = 'p' + str(i)
+        context_graph.add_node(node, pos=(2, i))
+    context_matrix = np.zeros((len(unranked_phrases), len(unranked_patterns)))
+    # find c (t, p)
+    with open(file, 'r') as f:
         t = f.read().lower()
         matcher = Matcher(nlp.vocab)
         doc = nlp(t)
-        for cp in context_pattern:
-            matcher.add("extraction", None, cp)
+        for i in range(len(unranked_patterns)):
+            matcher.add("extraction", None, unranked_patterns[i])
             matches = matcher(doc)
             for match_id, start, end in matches:
                 span = doc[start+2:end].text
-                if span not in new_phrases:
-                    new_phrases.add(span)
+                j = unranked_phrases.index(span)
+                context_matrix[j, i] += 1
             matcher.remove("extraction")
-    return new_phrases
+    # add context nodes into graph
+    c_count = 0
+    for i in range(context_matrix.shape[0]):
+        for j in range(context_matrix.shape[1]):
+            if context_matrix[i, j] != 0:
+                occur = context_matrix[i, j]
+                node_t = 't' + str(i)
+                node_p = 'p' + str(j)
+                node_c = 'c' + str(c_count)
+                c_count += 1
+                context_graph.add_node(node_c, pos=(1, c_count))
+                context_graph.add_edge(node_t, node_c, weight=occur)
+                context_graph.add_edge(node_c, node_p, weight=occur)
+    # draw context graph
+    pos=nx.get_node_attributes(context_graph,'pos')
+    nx.draw(context_graph, pos, with_labels=True)
+    labels = nx.get_edge_attributes(context_graph, 'weight')
+    nx.draw_networkx_edge_labels(context_graph,pos,edge_labels=labels)
+    # return patterns
+    return unranked_phrases
 
 #%%
 seed = set(['multimedia data types', 'database system', 'cryptographic algorithm'])
-context_p = patternSearch(seed, 'test.txt')
+new_phrases = patternSearch(seed, 'small.txt')
 # print(context_p)
-new_phrases = phraseExtraction('test.txt', context_p)
 print(new_phrases)
-
-new_p = patternSearch(new_phrases, 'test.txt')
-pnn = phraseExtraction('test.txt', new_p)
-print(pnn)
 
 # %%
