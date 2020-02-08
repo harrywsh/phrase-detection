55d38e2da1da666cb3c3e9f538c1d72675370b90
diproray <diprorayy@gmail.com>
2020-02-07 18:40:03 -0600

    Updated to include scoring method choice

---
+++ final_stuff/try_all_scoring_methods.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+python3 final_framework.py arxiv_titles_and_abstracts.txt 100 500 eval_score_0.txt 0
+python3 final_framework.py arxiv_titles_and_abstracts.txt 100 500 eval_score_1.txt 1
+python3 final_framework.py arxiv_titles_and_abstracts.txt 100 500 eval_score_2.txt 2
+python3 final_framework.py arxiv_titles_and_abstracts.txt 100 500 eval_score_3.txt 3
+python3 final_framework.py arxiv_titles_and_abstracts.txt 100 500 eval_score_4.txt 4
